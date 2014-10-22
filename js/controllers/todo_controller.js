27f101373d283137b2e530291287f55e08d68558
Alexandre Mercier <amercier@slideshare.com>
2014-10-22 11:44:18 -0700

    Fixed 'doneEditing' action getting sent twice

--- js/controllers/todo_controller.js
+++ js/controllers/todo_controller.js
@@ -8,7 +8,12 @@ Todos.TodoController = Ember.ObjectController.extend({
       this.set('isEditing', false);
 
       if (Ember.isEmpty(this.get('model.title'))) {
-        this.send('removeTodo');
+        // The `doneEditing` action gets sent twice when the user hits
+        // enter (once via 'insert-newline' and once via 'focus-out').
+        //
+        // We debounce our call to 'removeTodo' so that it only gets
+        // made once.
+        Ember.run.debounce(this, 'removeTodo', 0);
       } else {
         this.get('model').save();
       }
