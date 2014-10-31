47dca610d6b82e3f91abb08f09b1fa04c6d3f71e
Alexandre Mercier <amercier@slideshare.com>
2014-10-31 12:52:12 -0700

    Added more integration tests

--- tests/integration/todos.js
+++ tests/integration/todos.js
@@ -9,6 +9,7 @@ test('Application initializes correctly', function() {
   visit('/');
   andThen(function() {
     equal(find('h1').text(), 'todos');
+    Todos.reset();
   });
 });
 
@@ -16,10 +17,11 @@ test('Application displays the list of todos', function() {
   expect(4);
   visit('/');
   andThen(function() {
-    equal(find('section#main > ul > li').size(), 3);
-    equal(find('section#main > ul > li:nth-child(1) label').text(), 'Learn Ember.js');
-    equal(find('section#main > ul > li:nth-child(2) label').text(), '...');
-    equal(find('section#main > ul > li:nth-child(3) label').text(), 'Profit!');
+    equal(find('#main > ul > li').size(), 3);
+    equal(find('#main > ul > li:nth-child(1) label').text(), 'Learn Ember.js');
+    equal(find('#main > ul > li:nth-child(2) label').text(), '...');
+    equal(find('#main > ul > li:nth-child(3) label').text(), 'Profit!');
+    Todos.reset();
   });
 
 });
@@ -28,7 +30,8 @@ test('Application displays the list of active todos', function() {
   expect(1);
   visit('/active');
   andThen(function() {
-    equal(find('section#main > ul > li').size(), 2);
+    equal(find('#main > ul > li').size(), 2);
+    Todos.reset();
   });
 
 });
@@ -37,6 +40,28 @@ test('Application displays the list of completed todos', function() {
   expect(1);
   visit('/completed');
   andThen(function() {
-    equal(find('section#main > ul > li').size(), 1);
+    equal(find('#main > ul > li').size(), 1);
+    Todos.reset();
+  });
+});
+
+test('Ticking an active todo completes it', function() {
+  expect(2);
+  visit('/');
+  click('#main > ul > li:nth-child(2) input[type=checkbox]');
+  andThen(function() {
+    ok(find('#main > ul > li:nth-child(2)').hasClass('completed'));
+    ok(!find('#main > ul > li:nth-child(3)').hasClass('completed'));
+    Todos.reset();
+  });
+});
+
+test('Ticking an completed todo re-actives it', function() {
+  expect(1);
+  visit('/');
+  click('#main > ul > li:nth-child(1) input[type=checkbox]');
+  andThen(function() {
+    ok(!find('#main > ul > li:nth-child(1)').hasClass('completed'));
+    Todos.reset();
   });
 });
